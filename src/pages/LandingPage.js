1f1c424c434f769434974dbdf78f71a34e0a7953
thkcyber-ai <thkcyberai@gmail.com>
2025-12-24 11:58:34 -0500

    Use absolute video URL for PoV background stability

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -41,7 +41,7 @@ function LandingPage() {
           zIndex: 0
         }}
       >
-        <source src="/assets/ShesVerified.mp4" type="video/mp4" />
+        <source src="https://d005124a.kycshield-frontend.pages.dev/assets/shield-animation.mp4" type="video/mp4" />
       </video>
       <div style={{
         position: 'absolute',
